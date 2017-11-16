ac30f04afdaa9be19371930472e9d6d5119ae1d6
Cristian Lara <cristianlara@stanford.edu>
2017-11-16 07:11:29 -0800

    basic rubiks cube framework, brute force solution

---
+++ cube.py
@@ -0,0 +1,95 @@
+import copy
+
+class Cube:
+
+    SOLVED_CUBE = [
+        [ 0, 0, 0,
+          0, 0, 0,
+          0, 0, 0 ],
+
+        [ 1, 1, 1,
+          1, 1, 1,
+          1, 1, 1 ],
+
+        [ 2, 2, 2,
+          2, 2, 2,
+          2, 2, 2 ],
+
+        [ 3, 3, 3,
+          3, 3, 3,
+          3, 3, 3 ],
+
+        [ 4, 4, 4,
+          4, 4, 4,
+          4, 4, 4 ],
+
+        [ 5, 5, 5,
+          5, 5, 5,
+          5, 5, 5 ]
+    ]
+
+    def __init__(self, faces=SOLVED_CUBE):
+        self.faces = {}
+        self.faces['U'] = faces[0]
+        self.faces['L'] = faces[1]
+        self.faces['F'] = faces[2]
+        self.faces['R'] = faces[3]
+        self.faces['B'] = faces[4]
+        self.faces['D'] = faces[5]
+
+    def rotate(self, face, reverse=False):
+        faceCopy = list(self.faces[face])
+        order = [(0,6), (1,3), (2,0), (3,7), (4,4), (5,1), (6,8), (7,5), (8,2)]
+        # rotate the numbers on the face
+        for a, b in order:
+            if reverse is False:
+                self.faces[face][a] = faceCopy[b]
+            else:
+                self.faces[face][b] = faceCopy[a]
+
+        # rotate the numbers on adjacent faces
+        if face == 'U':
+            self.rotateAdjacent(['F', 'R', 'B', 'L'], reverse, order=[0, 1, 2])
+        elif face == 'D':
+            self.rotateAdjacent(['F', 'R', 'B', 'L'], reverse, order=[6, 7, 8])
+        elif face == 'F':
+            self.rotateAdjacent(['D', 'R', 'U', 'L'], reverse)
+        elif face == 'R':
+            self.rotateAdjacent(['D', 'B', 'U', 'F'], reverse)
+        elif face == 'B':
+            self.rotateAdjacent(['D', 'L', 'U', 'R'], reverse)
+        elif face == 'L':
+            self.rotateAdjacent(['D', 'F', 'U', 'B'], reverse)
+
+    def rotateAdjacent(self, faces, reverse, order=None):
+        facesCopy = copy.deepcopy(self.faces)
+
+        if order is not None:
+            for i, face in enumerate(faces):
+                for j in order:
+                    nextIndex = (i+1)%4
+                    if reverse is False:
+                        self.faces[face][j] = facesCopy[faces[nextIndex]][j]
+                    else:
+                        self.faces[faces[nextIndex]][j] = facesCopy[face][j]
+        else:
+            order = [[0, 1, 2], [6, 3, 0], [8, 7, 6], [2, 5, 8]]
+            for i, face in enumerate(faces):
+                for j in range(len(order[0])):
+                    nextIndex = (i+1)%4
+                    if reverse is False:
+                        self.faces[face][order[i][j]] = facesCopy[faces[nextIndex]][order[nextIndex][j]]
+                    else:
+                        self.faces[faces[nextIndex]][order[nextIndex][j]] = facesCopy[face][order[i][j]]
+
+    def isSolved(self):
+        faces = set(tuple(i) for i in self.faces.values())
+        return faces == set( tuple(i) for i in [ [i for j in range(9)] for i in range(6)] )
+
+
+def generateCube():
+    cube = Cube()
+    cube.rotate('F')
+    cube.rotate('U')
+    cube.rotate('R')
+    return cube
---
+++ solver.py
@@ -0,0 +1,40 @@
+import util
+import cube
+import copy
+
+class RubiksProblem:
+    def __init__(self, cube):
+        self.cube = cube
+        self.faces = ['L', 'R', 'F', 'B', 'U', 'D']
+
+    def startState(self):
+        return self.cube
+
+    def isEnd(self, cube):
+        return cube.isSolved()
+
+    def succAndCost(self, cube):
+        results = []
+        for face in self.faces:
+            for reverse in [False, True]:
+                cost = self.stateCost()
+                newCube = copy.deepcopy(cube)
+                newCube.rotate(face, reverse=reverse)
+                action = face
+                if reverse is True: action += "\'"
+                results.append((action, newCube, cost))
+        return results
+
+    def stateCost(self):
+        return 1
+
+
+def solve(query):
+    ucs = util.UniformCostSearch(verbose=0)
+    ucs.solve(RubiksProblem(query))
+    return 'Solution: ' + ', '.join(ucs.actions)
+
+if __name__ == '__main__':
+    cube = cube.generateCube()
+    solution = solve(cube)
+    print solution
